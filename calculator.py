d4e7a5300444783ac77b23b11b1623ffb3e0caa0
Sathwika0503 <118364351+Sathwika0503@users.noreply.github.com>
2022-11-20 11:42:14 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
-n1=int(input("Enter the first number : "))
-n2=int(input("Enter the second number : "))
+n1=float(input("Enter the first number : "))
+n2=float(input("Enter the second number : "))
 
 print("Operators List:\n1. +\n2. -\n3. *\n4. /\n5. %")
 
